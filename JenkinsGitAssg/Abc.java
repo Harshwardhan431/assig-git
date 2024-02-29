4cdd5f2446c719f321dcea4b03391085eff5e41f
Harshwardhan Atkare <81025973+Harshwardhan431@users.noreply.github.com>
2024-02-29 10:07:16 +0530

    final commit

--- JenkinsGitAssg/Abc.java
+++ JenkinsGitAssg/Abc.java
@@ -3,10 +3,6 @@ public class Abc
 	public static void main(String args[])
 	{
 		System.out.println("Hello Harsh");
-		System.out.println("Hello Yash");
-		System.out.println("Hello Sakshi");
-		System.out.println("Hello ");
-		System.out.println("Good morning");
-		System.out.println("Hi");
+		
 	}
 }
